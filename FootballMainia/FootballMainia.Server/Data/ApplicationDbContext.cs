cb43f573d0b8cc385764e3013cb9005859703320
Paweł Przewała <pp300330@student.polsl.pl>
2025-04-28 15:00:05 +0200

    AddStringToDatabase

--- FootballMainia/FootballMainia.Server/Data/ApplicationDbContext.cs
+++ FootballMainia/FootballMainia.Server/Data/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-
+using FootballMainia.Server.Models;
 namespace FootballMainia.Server.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<PlayersItem> Players { get; set; }
     }
 }
